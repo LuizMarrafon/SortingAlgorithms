25aa01d81111f3d1367f15dca8048d311f8086ec
gustavoimada <gu.imada@hotmail.com>
2026-03-22 11:46:10 -0300

    agr com o mais atualizado

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ void main() {
     //arquivo.shake_sort();
     //arquivo.quickSemPivo();
     //arquivo.quickComPivo();
-    arquivo.bucket_sort();
+    //arquivo.bucket_sort();
     //arquivo.merge_sort();
 
     System.out.println();
